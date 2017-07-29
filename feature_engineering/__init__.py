f252b44d8dbee5b04b7a88b69da0b513709c52de
MayukhSobo <mayukh2012@hotmail.com>
2017-07-29 21:36:25 +0530

    Converting feature engineering into package

---
+++ feature_engineering/__init__.py
@@ -0,0 +1,17 @@
+"""
+This package covers all the feature engineering logic.
+This includes the following things
+    1. Feature Lading form the pickle data
+    2. Feature framing and outlier removal
+    3. Feature scaling
+    4. Feature Creation
+    5. Feature Importance
+
+Usage Pattern:
+
+    1. Create a object of FeatureExtract
+    2. Use the object of FeatureExtract and pass it to Aux class
+    3. Use feature creation or feature scaling
+    4. Pass it to feature importance object
+    5. Call respective feature importance function according to algorithm.
+"""
